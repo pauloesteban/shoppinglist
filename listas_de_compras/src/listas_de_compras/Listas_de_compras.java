007860ab9975429fb2a2e47f85636e6c7febc56c
LUIS SOLÓRZANO <luique1996@gmail.com>
2016-11-04 15:24:32 -0500

    Avance 1.1 Commit continue
    El botón fue separa de la barra de búsqueda, se mejor el incio del
    pograma y el diseño del mismo

--- listas_de_compras/src/listas_de_compras/Listas_de_compras.java
+++ listas_de_compras/src/listas_de_compras/Listas_de_compras.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package listas_de_compras;
 
-/**
- *
- * @author LuisSolorzano
- */
 public class Listas_de_compras {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+      
         //welcome=new welcome();
     }
     
